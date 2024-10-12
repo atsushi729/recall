6ea31b117fe57fccde6349dfa731a46551ae1f47
Atsushi <atu729@icloud.com>
2024-10-12 11:31:06 +0900

    fix home layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,6 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { Button } from "~/components/button";
 import Hero from "~/components/hero";
-import { PaperClipIcon } from "@heroicons/react/24/solid";
-import SelectBox from "~/components/selectBox";
-import {
-  languageOptions,
-  numberOfQuestionOptions,
-  questionTypeOptions,
-} from "~/data/selectOptions";
+import MainForm from "~/components/mainForm";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,28 +14,7 @@ export default function Index() {
     <div className="relative bg-black min-h-screen bg-theme-base text-white flex flex-col items-center justify-center p-4">
       <div className="w-full max-w-3xl space-y-6">
         <Hero />
-        <div className="bg-gray-900 p-4 rounded-lg">
-          <div className="flex items-center space-x-2">
-            <Button variant="outline" size="icon" className="shrink-0">
-              <PaperClipIcon className="h-4 w-4" aria-hidden="true" />
-            </Button>
-            <input
-              placeholder="Ask v0 a question..."
-              className="flex-grow bg-transparent border-b border-gray-700 focus:border-gray-600 focus:outline-none p-2 text-sm"
-            />
-            <Button variant="default" className="shrink-0">
-              Submit
-            </Button>
-          </div>
-        </div>
-        <div className="flex items-center space-x-6">
-          <SelectBox options={languageOptions} label="Language" />
-          <SelectBox
-            options={numberOfQuestionOptions}
-            label="Number of Question"
-          />
-          <SelectBox options={questionTypeOptions} label="Question type" />
-        </div>
+        <MainForm />
       </div>
     </div>
   );
