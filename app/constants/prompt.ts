548e20f36ece1ee82af3920672c4ac514a45b0ba
Atsushi <atu729@icloud.com>
2024-10-14 12:07:18 +0900

    defined prompt type

--- app/constants/prompt.ts
+++ app/constants/prompt.ts
@@ -1,12 +1,9 @@
+import { SystemPromptParams, UserPromptParams } from "~/types/prompt";
+
 export const PROMPT = {
   chat: {
-    system: (
-      language: string | null,
-      numberOfQuestions: string | null,
-      questionType: string | null
-    ) =>
-      `You are a helpful and knowledgeable teacher. Based on the input data provided, can you create appropriate questions, similar to how a teacher would create questions for students? Please generate ${numberOfQuestions} questions in ${language}.  Additionally, please ensure the questions are of type ${questionType}. You should generate only questions. After that can you generate answers for the questions?`,
-    assistant: "Got it. I will create question by following condition.",
-    user: (keyword: string) => `Can you create question based on folling keyword ${keyword}`
+    system: (params: SystemPromptParams) => `You are a helpful and knowledgeable teacher. Based on the input data provided, can you create appropriate questions, similar to how a teacher would create questions for students? Please generate ${params.numberOfQuestions} questions in ${params.language}. Additionally, please ensure the questions are of type ${params.questionType}. You should generate only questions. After that, can you generate answers for the questions?`,
+    assistant: "Got it. I will create questions by following the conditions. Moreover, I will gererate answers following formmating rules. question {question} answer {answer}",
+    user: (keyword: UserPromptParams) => `Can you create questions based on the following keyword: "${keyword}"? Please generate questions and answers based on the following format: question {question} answer {answer}`,
   }
 };
---
+++ app/types/prompt.ts
@@ -0,0 +1,9 @@
+export interface SystemPromptParams {
+  language: string | null;
+  numberOfQuestions: string | null;
+  questionType: string | null;
+}
+
+export interface UserPromptParams {
+  keyword: string;
+}
